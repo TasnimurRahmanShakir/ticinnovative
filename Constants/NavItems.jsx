02cb6d265540f21b898cfb3323621e487bd7a822
TasnimurRahmanShakir <tasnim.bd.cse@gmail.com>
2025-11-19 23:17:45 +0600

    add nav items

---
+++ Constants/NavItems.jsx
@@ -0,0 +1,74 @@
+import { href } from "react-router-dom";
+
+export const navItems = [
+  {
+    id: 1,
+    title: "Home",
+    href: "/",
+  },
+  {
+    id: 2,
+    title: "Amazon FBA/FBM Prep Service",
+    href: "/amazon-service",
+  },
+  {
+    id: 3,
+    title: "WFS Prep Service",
+    href: "/wfs-service",
+  },
+  {
+    id: 4,
+    title: "Storage/ WareHouse",
+    href: "/storage-warehouse",
+  },
+  {
+    id: 5,
+    title: "Boxes or Cotton Supply",
+    href: "/boxes-cotton-supply",
+  },
+  {
+    id: 6,
+    title: "Sipping / Cargo USA & International ",
+    href: "/shipping",
+  },
+  {
+    id: 7,
+    title: "Courier & Delivery",
+    href: "/courier-delivery",
+  },
+  {
+    id: 8,
+    title: "Carton Handling",
+    href: "/carton-handling",
+  },
+  {
+    id: 9,
+    title: "Our Products",
+    href: "/our-products",
+  },
+  {
+    id: 10,
+    title: "Media",
+    href: "/media",
+  },
+  {
+    id: 11,
+    title: "About Us",
+    href: "/about-us",
+  },
+  {
+    id: 12,
+    title: "WareHouse",
+    href: "/warehouse",
+  },
+  {
+    id: 13,
+    title: "Payment",
+    href: "/payment",
+  },
+  {
+    id: 14,
+    title: "Contact Us",
+    href: "/contact-us",
+  },
+];
